67076cdb097d50f6e34493bd8796112390ff80dc
Wilfredo Murillo Gonzalez <wilmugo@gmail.com>
2018-09-02 20:20:01 -0400

    render row in table of shopping cart

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //Variables
-const courses = document.querySelector('#courses-list');
+const courses = document.querySelector('#courses-list'),
+  contentOfShoppingCar = document.querySelector('#cart-content tbody');
 
 //Listeners
 
@@ -26,5 +27,37 @@ function addCourseToShopCar(e) {
 
 // function to extract info to course you click
 function getInfoOfCourse(course) {
-  console.log(course);
+  //create object with info of course
+  const infoCourse = {
+    image: course.querySelector('img').src,
+    title: course.querySelector('h4').textContent,
+    price: course.querySelector('.price span').textContent,
+    id: course.querySelector('a').getAttribute('data-id')
+  };
+
+  //function for add object into shopping car
+  addItemToCar(infoCourse);
+}
+
+// add and render item course in shopping car
+function addItemToCar(course) {
+  // create <tr>
+  const row = document.createElement('tr');
+
+  //render row in table
+  row.innerHTML = `
+    <tr>
+      <td>
+        <img src = "${course.image}" width=100>
+      </td>
+      <td>${course.title}</td>
+      <td>${course.price}</td>
+      <td>
+        <a href="#" class="remove" data-id="${course.id}">X</a>
+      </td>
+    </tr>
+  `;
+
+  //add row to tbody content
+  contentOfShoppingCar.appendChild(row);
 }
